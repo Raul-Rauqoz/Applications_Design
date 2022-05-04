3305f13f7547816d5c5920239898e541e8988ad7
raul-rauqoz <raul.quinonez@telus.com>
2022-05-04 09:32:26 -0600

    feat: generators example

---
+++ generators_example.js
@@ -0,0 +1,31 @@
+// Example of generators
+function* prueba_gen_() {
+	let start = 0;
+	while (true) {
+		let gg = yield start;
+		start = gg;
+
+		switch (start) {
+			case 'hola':
+				console.log('que onda como vas');
+				break;
+			case 'yes':
+				console.log('yes en ingles');
+				break;
+			case 'nel':
+				console.log('nelson mandela');
+				break;
+
+			default:
+				console.log('eh?', start);
+				break;
+		}
+	}
+}
+
+let prueba = prueba_gen_();
+// prueba.next();
+// console.log(prueba.next('hola'));
+// console.log(prueba.next('yes'));
+// console.log(prueba.next('yes'));
+// console.log(prueba.next('nel'));
